95cc01e9533b40e86f75ca847e0dc32b855fb991
Shirley Gonzalez <shirleypamela1020@gmail.com>
2018-11-15 23:21:53 +0000

    First draft, worked on conditionals

--- js/script.js
+++ js/script.js
@@ -9,3 +9,29 @@
 
 // DOCUMENT READY FUNCTION BELOW
 
+var userChoice ;
+
+$("#shoot").click(function(){
+    userChoice = $("#input").val();
+     $("#userChoice").text(userChoice);
+     
+});
+
+var computerChoice;
+
+$("#shoot").click(function(){
+    
+     var randomAnswer;
+     randomAnswer= Math.random();
+     if (randomAnswer <.33){
+        $("#computerChoice").text("Rock");}
+    else if(randomAnswer<.66){
+        $("#computerChoice").text("Paper");}
+    else {
+        $("computerChoice").text("Scissor")
+    }
+
+
+});
+
+
